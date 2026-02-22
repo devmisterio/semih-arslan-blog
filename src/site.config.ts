c274ea99e37f0d4a0d4cc50bc20b249407589579
Semih Arslan <arslan_semih@yahoo.com>
2026-02-22 20:19:01 +0300

    feat: Update social links with new styling, adjust header text size, and configure site URL.

--- src/site.config.ts
+++ src/site.config.ts
@@ -27,7 +27,7 @@ export const siteConfig: SiteConfig = {
 	*/
 	title: "Semih Arslan — Systems & Software",
 	// ! Please remember to replace the following site property with your own domain, used in astro.config.ts
-	url: "https://astro-cactus.chriswilliams.dev/",
+	url: "https://arslansemih.com",
 };
 
 // Used to generate links in both the Header & Footer.
